c890ef0aa87db25b4d2168e63d087ed8953a1ab0
Aliaa <aliaaadela22@gmail.com>
2025-09-28 15:14:52 +0300

    12 done

---
+++ Session12_home_work/reverse-linked-list.dart
@@ -0,0 +1,20 @@
+  class ListNode {
+    int val;
+    ListNode? next;
+    ListNode([this.val = 0, this.next]);
+  }
+ 
+class Solution {
+  ListNode? reverseList(ListNode? head) {
+       ListNode? previous = null;
+        ListNode? current = head;
+        while(current != null){
+            ListNode? next = current.next; 
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+    return  previous;
+  }
+}
+  
